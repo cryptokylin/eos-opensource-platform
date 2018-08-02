c58685b34b6fe4365c65845435fffddc90c54d32
qiushaoxi <qiushaoxi@163.com>
2018-08-03 02:08:50 +0800

    fix shell

--- start-compiler.sh
+++ start-compiler.sh
@@ -1,14 +1,17 @@
 echo "install jq tool first."
 
 image=`cat config.json | jq .compiler.image |sed 's/\"//g' `
-versions=`cat config.json | jq .compiler.versions |sed 's/\"//g' `
+length=`cat config.json | jq '.compiler.versions | length'`
 container=`cat config.json | jq .compiler.container |sed 's/\"//g' `
 
 echo $image
-echo $version
+echo $versions
 echo $container
 
-for version in $versions
+for(( i = 0; i <$length; i++ ))
 do
-   docker run -d -v `pwd`/contracts:/opt/contracts --name=$container-$version $image:$version /bin/sh -c "while true; do echo running; sleep 1; done"
+    echo $i
+    version=`cat config.json | jq .compiler.versions[$i] |sed 's/\"//g'`
+    echo $version
+    docker run -d -v `pwd`/contracts:/opt/contracts --name=$container-$version $image:$version /bin/sh -c "while true; do echo running; sleep 1; done"
 done
